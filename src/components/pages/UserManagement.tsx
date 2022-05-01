a3e951cf56ca1498c084f46ddba23cd2bd8330fd
kuwasin <76948644+kuwasin@users.noreply.github.com>
2022-05-02 00:12:16 +0900

    2022/5/1

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,56 @@
-import { FC, memo } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import {
+  Center,
+  Spinner,
+  useDisclosure,
+  Wrap,
+  WrapItem
+} from "@chakra-ui/react";
+import { FC, memo, useCallback, useEffect } from "react";
+
+import { UserDetailModal } from "../organisms/user/UserDetailModal";
+import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
+import { useSelectUser } from "../../hooks/useSelectUser";
 
 export const UserManagement: FC = memo(() => {
-  return <p>ユーザー管理ページです</p>;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { getUsers, users, loading } = useAllUsers();
+  const { onSelectUser, selectedUser } = useSelectUser();
+
+  useEffect(() => getUsers(), []);
+
+  const onClickUser = useCallback(
+    (id: number) => {
+      console.log(id);
+      onSelectUser({ id, users, onOpen });
+      onOpen();
+    },
+    [users, onSelectUser, onOpen]
+  );
+
+  return (
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                id={user.id}
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+                onClick={onClickUser}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
+    </>
+  );
 });
